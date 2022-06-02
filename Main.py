44654ecc21e94d8e729c4b376b9859549ee1a958
Mute <40270424+Ced0@users.noreply.github.com>
2022-06-02 20:42:00 +0200

    Modifying arg options system

--- Main.py
+++ Main.py
@@ -2,36 +2,65 @@ import os
 import sys
 import glob
 
-if(len(sys.argv) < 2):
-    exit()
+image = ""
+rgb = ""
+n = ""
+periodic = ""
+rotations = ""
+xml = "samples.xml"
+wfc = "true"
 
-file = open('samples.xml', 'r+')
-file.seek(0, os.SEEK_SET)
-fileContent = file.read().split('"')
+for i in range(1, len(sys.argv)):
+    opt = sys.argv[i].split("=")
 
-file.seek(0, os.SEEK_SET)
-file.write(fileContent[0])
-file.write('"')
-file.write(sys.argv[1])
-file.write('"')
+    if opt[0] == "image":
+        image = opt[1] 
+    elif opt[0] == "rgb":
+        rgb = opt[1]
+    elif opt[0] == "n":
+        n = opt[1]
+    elif opt[0] == "periodic":
+        periodic = opt[1]
+    elif opt[0] == "rotations":
+        rotations = opt[1]
+    elif opt[0] == "xml":
+        xml = opt[1]
+    elif opt[0] == "wfc":
+        wfc = opt[1]
+        
+if wfc == "true":
+    if image != "" or n != "" or periodic != "" or rotations != "":
+        file = open(xml, 'r+')
+        file.seek(0, os.SEEK_SET)
+        fileContent = file.read().split('"')
 
-for i in range(2, len(fileContent)):
-    if (i % 2) != 0: 
-        file.write('"')
-    file.write(fileContent[i])
-    if (i % 2) != 0:
-        file.write('"')
+        file.close()
+        file = open(xml, 'w+')
+        file.write(fileContent[0])
 
-file.close()
+        for i in range(1, len(fileContent)):
+            if (i % 2) != 0: 
+                file.write('"')
 
-os.system('python ./wfc_2019f-master/wfc_run.py -s samples.xml')
+            if i == 1 and image != "":
+                file.write(image)
+            elif i == 3 and n != "":
+                file.write(n)
+            elif i == 5 and periodic != "":
+                file.write(periodic)
+            elif i == 7 and rotations != "":
+                file.write(rotations)
+            else:
+                file.write(fileContent[i])
+
+            if (i % 2) != 0:
+                file.write('"')
+
+        file.close()
+
+    os.system('python ./wfc_2019f-master/wfc_run.py -s ' + xml)
 
 list_of_files = glob.glob('./output/*.png')
 latest_file = max(list_of_files, key=os.path.getctime)
-
-rgb = ""
-
-if(len(sys.argv) > 2):
-    rgb = sys.argv[2]
 
 os.system('python ./map_generation/src/Main.py ' + latest_file + " " + rgb)
