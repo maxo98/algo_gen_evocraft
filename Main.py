e85b88f9d66e2b05ebed85bbdd88d3ee023eaaa2
Mute <40270424+Ced0@users.noreply.github.com>
2022-06-02 19:39:43 +0200

    Choose image from cmd arg

--- Main.py
+++ Main.py
@@ -5,7 +5,26 @@ import glob
 if(len(sys.argv) < 2):
     exit()
 
-os.system('python ./wfc_2019f-master/wfc_run.py -s ' + sys.argv[1])
+file = open('samples.xml', 'r+')
+file.seek(0, os.SEEK_SET)
+fileContent = file.read().split('"')
+
+file.seek(0, os.SEEK_SET)
+file.write(fileContent[0])
+file.write('"')
+file.write(sys.argv[1])
+file.write('"')
+
+for i in range(2, len(fileContent)):
+    if (i % 2) != 0: 
+        file.write('"')
+    file.write(fileContent[i])
+    if (i % 2) != 0:
+        file.write('"')
+
+file.close()
+
+os.system('python ./wfc_2019f-master/wfc_run.py -s samples.xml')
 
 list_of_files = glob.glob('./output/*.png')
 latest_file = max(list_of_files, key=os.path.getctime)
